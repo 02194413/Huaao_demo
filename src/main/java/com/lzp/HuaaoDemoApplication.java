d4837c9a8c6298927af2052cdf30a76dca98c023
new cosmos <3051097568@qq.com>
2026-07-11 20:22:27 +0800

    init: project scaffold

---
+++ src/main/java/com/lzp/HuaaoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaaoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaaoDemoApplication.class, args);
+    }
+
+}
